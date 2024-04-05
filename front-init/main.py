683dd77cf0057b63d9a8b12efb2b58915f14e9b9
belarr0 <belarr00ss@gmail.com>
2024-04-05 13:44:10 +0300

    add processing of static resources

--- front-init/main.py
+++ front-init/main.py
@@ -12,12 +12,23 @@ class HttpHandler(BaseHTTPRequestHandler):
             self.send_html_file('index.html')
         elif pr_url.path == '/message':
             self.send_html_file('message.html')
+        elif pr_url.path == '/style.css':
+            self.send_static_file('style.css', 'text/css')
+        elif pr_url.path == '/logo.png':
+            self.send_static_file('logo.png', 'image/png')
         else:
             self.send_html_file('error.html', 404)
 
     def send_html_file(self, filename, status=200):
         self.send_response(status)
         self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        with open(filename, 'rb') as fd:
+            self.wfile.write(fd.read())
+
+    def send_static_file(self, filename, content_type):
+        self.send_response(200)
+        self.send_header('Content-type', content_type)
         self.end_headers()
         with open(filename, 'rb') as fd:
             self.wfile.write(fd.read())
@@ -38,7 +49,7 @@ def run_http_server():
     httpd = HTTPServer(server_address, HttpHandler)
     print('HTTP server started on port 3000\nhttp://localhost:3000/')
     httpd.serve_forever()
-
+ййй
 def run_socket_server():
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as server_socket:
         server_socket.bind(('localhost', 5000))
@@ -46,9 +57,10 @@ def run_socket_server():
         while True:
             data, addr = server_socket.recvfrom(1024)
             data_dict = json.loads(data.decode())
-            with open('storage/data.json', 'a') as file:
+            with open('storage/data.json', 'w') as file:
                 json.dump({data_dict['timestamp']: {'username': data_dict['username'], 'message': data_dict['message']}}, file, indent=2)
                 file.write('\n')
+
 
 if __name__ == '__main__':
     http_thread = threading.Thread(target=run_http_server)
